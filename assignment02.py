33444314f9aae2f835decda4b7647998b1e09dbd
JubileeXie <13858085137xxf/>
2019-10-22 16:14:32 +0200

    add sth

--- assignment02.py
+++ assignment02.py
@@ -1,4 +1,12 @@
+# Xingfang Xie
+# 22/10/2019
+# Assignmment 02
 
-#we are writing a script
+import math
+
+# Exercise 1 sanity check
+# Question 1: How many scenes from each individual sensor are located in the folders?
+
+# Count how many scenes start with 'LC08'?
 
 
