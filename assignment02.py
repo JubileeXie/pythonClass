3330d3d6875f02012cddad064322b6a3a2a3e097
JubileeXie <13858085137xxf/>
2019-10-22 16:35:04 +0200

    all the questions

--- assignment02.py
+++ assignment02.py
@@ -7,6 +7,28 @@ import math
 # Exercise 1 sanity check
 # Question 1: How many scenes from each individual sensor are located in the folders?
 
-# Count how many scenes start with 'LC08'?
+
+# Count how many scenes start with 'LC08', 'LE07', 'LT05' , 'LT04' in each folders
+
+
+# Question 2: Count the number of erroneous scenes (do not have the “correct” number of files in them)
+
+
+# Question 3: Upload a txt-file (each corrupt scene is written as an individual line)
+
+
+# Question 4: Count number of layers for both types (vector and raster)
+
+
+# Question 5: Count number of incomplete vector and raster layers
+
+
+# Question 6: provide a list of the layer names
+
+
+
+
+
+
 
 
